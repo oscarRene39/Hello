1811d6313951b80c8d85a4d78495877946126c05
oscar rene peñaloza ardila <oscar.penaloza@est.iudigital.edu.co>
2026-04-13 07:51:05 -0500

    list and tuple

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-print("Hola, Oscar!")
-print("This is CS50P")
+from os import name
+
+
+def main():
+    print("Hola, Oscar!")
+    print("This is CS50P")
+    # Definiendo una lista y una tupla
+    nameList = ["Oscar","Carol","Mynna","Dominick"," esto es una lista"]
+    print(nameList)
+    print(" ".join(nameList))
+    nametuple = ("Oscar","Carol","Mynna","Dominick"," esto es una tupla")
+    print(nametuple)
+    print(" ".join(nametuple))
+    """faltan ejemplos de set y diccionarios"""    
+
+main()
